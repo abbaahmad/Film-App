b4f6e86830b3432531d3ec62db78609c97aa61bb
abbaahmad <57046949+abbaahmad@users.noreply.github.com>
2019-10-30 23:10:46 +0300

    Update Login.java

--- Login.java
+++ Login.java
@@ -1,14 +1,12 @@
 package com.accenture.itfactory.base.FilmApplication;
 
 import java.util.*;
-//import java.util.List;
 
 public class Login {
     private List<User> userList;
     List<User> adminList;
     HashSet<User> userSet;
     Map<String,String> userData;
-    //protected static int num_of_users;
     protected static boolean isInUserList;
 
     public Login()
@@ -18,29 +16,12 @@ public class Login {
         userSet = new HashSet<>();
         userData = new HashMap<>();
         isInUserList = false;
-        //num_of_users = 0;
     }
     public void addUser(User newUser){
-        //userList.add(newUser);
         userSet.add(newUser);
         if(userSet.contains(newUser))
             userData.put(newUser.login,newUser.password);
-        //num_of_users++;
     }
-    /*public void addUser(String newName,String newLogin,String newPassword) throws Exception {
-        try{
-            if(isUser(newLogin,newPassword)) {
-                userList.add(new User());
-                userList.get(userList.size() - 1).name = newName;
-                userList.get(userList.size() - 1).login = newLogin;
-                userList.get(userList.size() - 1).password = newPassword;
-                //num_of_users++;
-            }
-        }
-        catch (final Exception e){
-            throw new Exception("User already exist",e);
-        }
-    }*/
     public void addUser(String newName,String newLogin,String newPassword){
         User newUser = new User(newName,newLogin,newPassword);
         userSet.add(newUser);
@@ -71,44 +52,6 @@ public class Login {
         return result;
     }
 
-    /*public /*String*//* boolean isUser(String userLogin,String userPassword) throws Exception {
-        boolean correctLogin = false;
-        boolean correctPassword = false;
-        String message = "";
-        try{
-            if(userData.containsKey(userLogin)){
-                if(userData.get(userLogin).equals(userPassword))
-                    /*message = "Welcome back";/*//*correctLogin = correctPassword = true;
-                else{
-                    //message = "Wrong password";
-                    correctLogin = true;
-                    correctPassword = false;
-                }
-            }
-        }
-        catch (Exception e){
-            throw new Exception("Wrong password");
-        }
-        return message;
-        //if(correctPassword && correctLogin)
-        //    return true;
-        //else
-        //    return false;
-    }*/
-    /*public boolean isUser(String userLogin,String userPassword){
-        boolean isCorrectInputs = false;
-        for(User u : userList){
-            if(u.login.equals(userLogin)) {
-                isInUserList = true;
-                if(u.password.equals(userPassword))
-                    isCorrectInputs = true;
-                else
-                    isCorrectInputs = false;
-            }
-        }
-        return isCorrectInputs;
-    }*/
-
     public boolean isAdmin(String adminLogin,String adminPassword){
         boolean isCorrectInputs = false;
         for(User u : adminList){
@@ -121,16 +64,5 @@ public class Login {
         }
         return isCorrectInputs;
     }
-    /* public void addAdmin(User new_admin){
-        adminList.add(new_admin);
-        num_of_users++;
-    }*/
-    /*public void addAdmin(String new_admin_name,String new_login,String new_password){
-        adminList.add(new User());
-        adminList.get(adminList.size()-1).name = new_admin_name;
-        adminList.get(adminList.size()-1).login = new_login;
-        adminList.get(adminList.size()-1).password = new_password;
-    }*/
     public List<User> getUserList(){return userList;}
-    //public List<User> getAdminList() {return adminList;}
 }
